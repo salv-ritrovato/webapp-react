8c47a91c8ebbdfc6656da0dfba4173f6466f3ebd
salv-ritrovato <salv.ritrovato@gmail.com>
2026-04-24 10:39:40 +0200

    wrapped whole app App with LoaderProvider context

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DefaultLayout from "./layout/DefaultLayout";
 import HomePage from "./pages/HomePage";
 import MovieDetail from "./pages/MovieDetail";
+import { LoaderProvider } from "./contexts/LoaderContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies/:id" element={<MovieDetail />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <LoaderProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies/:id" element={<MovieDetail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </LoaderProvider>
   );
 }
 
